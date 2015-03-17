17395435c0a1c41f394a2761b6317abe7fed766b
Ahmedul Haque Abid <a_h_abid@hotmail.com>
2015-03-17 23:24:26 +0600

    Use Composer for modules autoloading.

--- module/Blog/Module.php
+++ module/Blog/Module.php
@@ -1,26 +1,16 @@
 <?php namespace Blog;
 
-use Blog\Model\Blog;
-use Blog\Model\BlogTable;
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
-use Zend\Db\ResultSet\ResultSet;
-use Zend\Db\TableGateway\TableGateway;
 
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+class Module implements ConfigProviderInterface
 {
-    public function getAutoloaderConfig()
-    {
-        
-    }
-
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
-    public function getServiceConfig()
-    {
-        return array();
-    }
+    // public function getServiceConfig()
+    // {
+    //     return array();
+    // }
 }
